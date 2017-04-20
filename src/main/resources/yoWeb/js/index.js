33313b99af46d4672c7ea9d582072709234ac30c
Joel Dudley <joel.dudley@r3cev.com>
2017-04-20 10:27:05 +0100

    Handles sending of messages.

--- src/main/resources/yoWeb/js/index.js
+++ src/main/resources/yoWeb/js/index.js
@@ -2,26 +2,26 @@
 
 const app = angular.module('yoModule', ['ui.bootstrap']);
 
-app.controller('yoCtrl', function($http) {
+app.controller('yoCtrl', function($http, $interval) {
     const yoApp = this;
+    // TODO: Change this back to a relative path.
     const apiBaseURL = "http://localhost:10007/api/yo/";
 
     $http.get(apiBaseURL + "me").then((response) => yoApp.thisNode = response.data.me);
     $http.get(apiBaseURL + "peers").then((response) => {
         yoApp.peers = response.data.peers;
-        yoApp.yos = {}
-        yoApp.peers.forEach(function(peer) {
-            yoApp.yos[peer] = [];
-        });
-        yoApp.updateYos();
-        console.log(yoApp.yos)
     });
 
     yoApp.updateYos = () => {
         $http.get(apiBaseURL + "yos").then((response) => {
+            console.log(response);
+            yoApp.yos = {}
+            yoApp.peers.forEach(function(peer) {
+                yoApp.yos[peer] = [];
+            });
+
             response.data.forEach(function (data) {
                 const yo = data.state.data;
-
                 let counterparty = yo.target;
                 let sender = "Me: ";
                 if (yo.origin != yoApp.thisNode) {
@@ -29,8 +29,16 @@ app.controller('yoCtrl', function($http) {
                     sender = "Them: ";
                 }
 
-                yoApp.yos[counterparty].push([sender, yo.yo]);
+                yoApp.yos[counterparty].unshift([sender, yo.yo]);
             });
         });
     };
+
+    yoApp.send = (recipient) => {
+        const sendMsgEndpoint = apiBaseURL + `yo?target=${recipient}&message=${yoApp.form.message}`;
+        $http.get(sendMsgEndpoint).then(() => {}, () => {});
+        yoApp.form.message = "";
+    }
+
+    var promise = $interval(yoApp.updateYos, 100);
 });
